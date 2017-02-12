fd2dbd39262fba9c89e2f164c697c9b20181f3a9
KirillVS <123@example.com>
2017-02-12 13:59:07 +0300

    Начало

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,59 @@
+<?php
+
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number2")
+     */
+    public function numberAction()
+    {
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(19.99);
+        $product->setDescription('Ergonomic and stylish!');
+        $product->setDescription2('Ergonomic and stylish!');
+        $em = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+
+        ////////////////////
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT p
+            FROM AppBundle:Product p
+            WHERE p.price > :price
+            ORDER BY p.price ASC'
+        )->setParameter('price', 15);
+
+        $products = $query->getResult();
+        //var_dump($products);
+        ///////////////////
+
+
+
+        $number = mt_rand(0, 100);
+
+        /*return new Response(
+            '<html><body>Lucky number: '.$number.' and ID'.$product->getId().'</body></html>'
+        );*/
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
